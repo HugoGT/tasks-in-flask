7d80909c2b270a5e559e52a2b874f493649b6161
Hugo <gthugo@outlook.com>
2022-06-26 12:04:19 -0500

    Adding bootstrap to the proyect

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, make_response, redirect, render_template
-
+from flask_bootstrap import Bootstrap
 
 to_do_list = ['Hacer curso de Flask', 'Hacer la compra', 'Hacer examen de carrera']
 
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'Clave secreta'
 
 
 @app.errorhandler(404)
